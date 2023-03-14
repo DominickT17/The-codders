89c79e50e864cd9110f36cd98dac3fc02d484343
DominickT17 <124642279+DominickT17@users.noreply.github.com>
2023-03-14 09:52:40 -0600

    Actualice El Código De Mi Programa

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ num1 = int(input('Ingrese un número entero: '))
 num2 = int(input('Ingrese otro número entero: '))
 if num1 > num2:
     mayor = num1
+    print('El número mayor es:', num1)
 elif num2 > num1:
     mayor = num2
+    print('El número mayor es:', num2)
 #2
 #3
